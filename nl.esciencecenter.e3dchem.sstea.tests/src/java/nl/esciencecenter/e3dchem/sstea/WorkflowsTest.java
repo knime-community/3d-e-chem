f52f14ec7d4cbfa3b5e3de2bda33bb5b04b35115
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2016-06-17 12:45:44 +0200

    Merged integration tests module into tests module.
    (This commit was moved from https://github.com/3D-e-Chem/knime-sstea. Original id: 17fbe507ce47a88b37291e2f8640696d257d1dcb)

---
+++ nl.esciencecenter.e3dchem.sstea.tests/src/java/nl/esciencecenter/e3dchem/sstea/WorkflowsTest.java
@@ -0,0 +1,48 @@
+package nl.esciencecenter.e3dchem.sstea;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.workflow.UnsupportedWorkflowVersionException;
+import org.knime.core.util.LockFailedException;
+import org.knime.testing.core.TestrunConfiguration;
+
+import nl.esciencecenter.e3dchem.knime.testing.TestFlowRunner;
+
+public class WorkflowsTest {
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	private TestFlowRunner runner;
+
+	@Before
+	public void setUp() {
+		TestrunConfiguration runConfiguration = new TestrunConfiguration();
+		runConfiguration.setTestDialogs(true);
+		runConfiguration.setReportDeprecatedNodes(true);
+		runConfiguration.setCheckMemoryLeaks(true);
+		runner = new TestFlowRunner(collector, runConfiguration);
+	}
+
+	@Test
+	public void test_simple() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		File workflowDir = new File("src/knime/sstea-simple-test");
+		runner.runTestWorkflow(workflowDir);
+	}
+	//
+	// @Test
+	// public void test_simply_broken() throws IOException,
+	// InvalidSettingsException, CanceledExecutionException,
+	// UnsupportedWorkflowVersionException, LockFailedException,
+	// InterruptedException {
+	// File workflowDir = new File("src/knime/simply-broken");
+	// runner.runTestWorkflow(workflowDir);
+	// }
+
+}
