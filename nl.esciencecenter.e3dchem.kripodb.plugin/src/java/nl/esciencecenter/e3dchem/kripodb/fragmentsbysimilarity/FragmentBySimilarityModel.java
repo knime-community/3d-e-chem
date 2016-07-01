781784cebe2b8e24ea10ed5ec899f950457ed71c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-07-01 17:10:34 +0200

    Improved dialog validation in configure() methods.
    (This commit was moved from https://github.com/3D-e-Chem/knime-kripodb. Original id: 30fdc46da0727505493dca38f7078b8192a2eee3)

--- nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/fragmentsbysimilarity/FragmentBySimilarityModel.java
+++ nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/fragmentsbysimilarity/FragmentBySimilarityModel.java
@@ -1,10 +1,13 @@
 package nl.esciencecenter.e3dchem.kripodb.fragmentsbysimilarity;
 
+import java.io.File;
+
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.def.DoubleCell;
 import org.knime.core.data.def.StringCell;
 import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.port.PortType;
 
 import nl.esciencecenter.e3dchem.kripodb.PythonNodeModel;
@@ -14,23 +17,47 @@ import nl.esciencecenter.e3dchem.kripodb.PythonNodeModel;
  *
  */
 public class FragmentBySimilarityModel extends PythonNodeModel<FragmentsBySimilarityConfig> {
-	public FragmentBySimilarityModel() {
-		super(new PortType[] { BufferedDataTable.TYPE }, new PortType[] { BufferedDataTable.TYPE });
-		python_code_filename = "fragment_by_similarity.py";
-	}
+    public FragmentBySimilarityModel() {
+        super(new PortType[] { BufferedDataTable.TYPE }, new PortType[] { BufferedDataTable.TYPE });
+        python_code_filename = "fragment_by_similarity.py";
+    }
 
-	@Override
-	protected FragmentsBySimilarityConfig createConfig() {
-		return new FragmentsBySimilarityConfig();
-	}
+    @Override
+    protected FragmentsBySimilarityConfig createConfig() {
+        return new FragmentsBySimilarityConfig();
+    }
 
-	@Override
-	protected DataTableSpec[] getOutputSpecs(DataTableSpec[] inSpecs) {
-		DataTableSpec outputSpec = new DataTableSpec(
-				new DataColumnSpecCreator("hit_frag_id", StringCell.TYPE).createSpec(),
-				new DataColumnSpecCreator("query_frag_id", StringCell.TYPE).createSpec(),
-				new DataColumnSpecCreator("score", DoubleCell.TYPE).createSpec());
-		return new DataTableSpec[] { outputSpec };
-	}
+    @Override
+    protected DataTableSpec[] getOutputSpecs(DataTableSpec[] inSpecs) {
+        DataTableSpec outputSpec = new DataTableSpec(new DataColumnSpecCreator("hit_frag_id", StringCell.TYPE).createSpec(),
+                new DataColumnSpecCreator("query_frag_id", StringCell.TYPE).createSpec(),
+                new DataColumnSpecCreator("score", DoubleCell.TYPE).createSpec());
+        return new DataTableSpec[] { outputSpec };
+    }
+
+    @Override
+    protected DataTableSpec[] configure(DataTableSpec[] inSpecs) throws InvalidSettingsException {
+        // TODO test if kripodb Python package is installed
+        FragmentsBySimilarityConfig config = getConfig();
+        int idColumnIndex = inSpecs[0].findColumnIndex(config.getFragmentIdColumn().getStringValue());
+        if (idColumnIndex < 0) {
+            throw new InvalidSettingsException("No valid fragment identifier column selected, require a String column");
+        }
+
+        String matrix = config.getMatrix().getStringValue();
+        if (matrix == "" || matrix == null) {
+            throw new InvalidSettingsException("Require a matrix file or ws url");
+        } else {
+            if (matrix.startsWith("http")) {
+                // TODO test if webservice is online, using a HEAD request.
+            } else {
+                File fragmentsdb = new File(matrix);
+                if (!fragmentsdb.canRead()) {
+                    throw new InvalidSettingsException("Unable to read matrix file");
+                }
+            }
+        }
+        return super.configure(inSpecs);
+    }
 
 }
