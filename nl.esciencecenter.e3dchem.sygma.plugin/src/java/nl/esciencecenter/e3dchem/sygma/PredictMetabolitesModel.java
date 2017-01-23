60e12a8e504645eef87798ab854d670843741376
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-01-23 10:55:56 +0100

    Added sygma Python installation instruction in node description.
    Made sygma Python package not found error message more descriptive.
    
    Fixes #8
    
    
    (This commit was moved from https://github.com/3D-e-Chem/knime-sygma. Original id: 8b35d4cc9e2d2198bcdb2b3c6bf87bc562b69f04)

--- nl.esciencecenter.e3dchem.sygma.plugin/src/java/nl/esciencecenter/e3dchem/sygma/PredictMetabolitesModel.java
+++ nl.esciencecenter.e3dchem.sygma.plugin/src/java/nl/esciencecenter/e3dchem/sygma/PredictMetabolitesModel.java
@@ -14,33 +14,48 @@ import nl.esciencecenter.e3dchem.python.PythonWrapperNodeModel;
  *
  */
 public class PredictMetabolitesModel extends PythonWrapperNodeModel<PredictMetabolitesConfig> {
-    /**
-     * Constructor for the node model.
-     */
-    protected PredictMetabolitesModel() {
-        // TODO one incoming port and one outgoing port is assumed
-        super(new PortType[] { BufferedDataTable.TYPE }, new PortType[] { BufferedDataTable.TYPE });
-        python_code_filename = "predict_metabolite.py";
-        required_python_packages = Arrays.asList("sygma");
-    }
+	/**
+	 * Constructor for the node model.
+	 */
+	protected PredictMetabolitesModel() {
+		// TODO one incoming port and one outgoing port is assumed
+		super(new PortType[] { BufferedDataTable.TYPE }, new PortType[] { BufferedDataTable.TYPE });
+		python_code_filename = "predict_metabolite.py";
+		required_python_packages = Arrays.asList("sygma");
+	}
 
-    @Override
-    protected PredictMetabolitesConfig createConfig() {
-        return new PredictMetabolitesConfig();
-    }
+	@Override
+	protected PredictMetabolitesConfig createConfig() {
+		return new PredictMetabolitesConfig();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected DataTableSpec[] configure(DataTableSpec[] inSpecs) throws InvalidSettingsException {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected DataTableSpec[] configure(DataTableSpec[] inSpecs) throws InvalidSettingsException {
 
-        // TODO: check if user settings are available, fit to the incoming
-        // table structure, and the incoming types are feasible for the node
-        // to execute. If the node can execute in its current state return
-        // the spec of its output data table(s) (if you can, otherwise an array
-        // with null elements), or throw an exception with a useful user message
+		// TODO: check if user settings are available, fit to the incoming
+		// table structure, and the incoming types are feasible for the node
+		// to execute. If the node can execute in its current state return
+		// the spec of its output data table(s) (if you can, otherwise an array
+		// with null elements), or throw an exception with a useful user message
 
-        return super.configure(inSpecs);
-    }
+		return super.configure(inSpecs);
+	}
+
+	@Override
+	public void validPython() throws InvalidSettingsException {
+		try {
+			super.validPython();
+		} catch (InvalidSettingsException e) {
+			if (e.getMessage().contains("install")) {
+				throw new InvalidSettingsException(
+						"'sygma' Python package not found, please install or correct Python executable, see the description of the SyGMa Metabolites node for help",
+						e);
+			}
+			throw e;
+		}
+
+	}
 }
