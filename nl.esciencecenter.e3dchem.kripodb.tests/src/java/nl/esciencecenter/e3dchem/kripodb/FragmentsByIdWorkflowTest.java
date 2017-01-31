2f1524a8b4a8e57ab74f2eb68300c63e3c892001
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-01-31 16:03:31 +0100

    Use relative path to input files in test workflows.
    Refs #20
    
    
    (This commit was moved from https://github.com/3D-e-Chem/knime-kripodb. Original id: 5d58b4d9678ca02866164799da980520c2a70d01)

--- nl.esciencecenter.e3dchem.kripodb.tests/src/java/nl/esciencecenter/e3dchem/kripodb/FragmentsByIdWorkflowTest.java
+++ nl.esciencecenter.e3dchem.kripodb.tests/src/java/nl/esciencecenter/e3dchem/kripodb/FragmentsByIdWorkflowTest.java
@@ -22,48 +22,53 @@ import nl.esciencecenter.e3dchem.knime.testing.TestFlowRunner;
 import nl.esciencecenter.e3dchem.python.PythonWrapperTestUtils;
 
 public class FragmentsByIdWorkflowTest {
-    @Rule
-    public ErrorCollector collector = new ErrorCollector();
-    private TestFlowRunner runner;
-    private static File fragments = new File(System.getProperty("java.io.tmpdir"), "fragments.sqlite");
+	private static final String FRAGMENTS_SQLITE = "./fragments.sqlite";
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	private TestFlowRunner runner;
+	private static File fragments = new File(FRAGMENTS_SQLITE);
 
-    @Before
-    public void setUp() {
-        TestrunConfiguration runConfiguration = new TestrunConfiguration();
-        runConfiguration.setTestDialogs(true);
-        runConfiguration.setAllowedMemoryIncrease(4096000);
-        runner = new TestFlowRunner(collector, runConfiguration);
-    }
+	@Before
+	public void setUp() {
+		TestrunConfiguration runConfiguration = new TestrunConfiguration();
+		runConfiguration.setTestDialogs(true);
+		runConfiguration.setAllowedMemoryIncrease(4096000);
+		runner = new TestFlowRunner(collector, runConfiguration);
+	}
 
-    @BeforeClass
-    public static void setUpDatafiles() throws MalformedURLException, IOException {
-        PythonWrapperTestUtils.materializeKNIMEPythonUtils();
-        FileUtils.copyURLToFile(new URL("https://github.com/3D-e-Chem/kripodb/raw/master/data/fragments.sqlite"), fragments);
-    }
+	private void runTestWorkflow(String wfDir) throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		File workflowDir = new File(wfDir);
+		runner.runTestWorkflow(workflowDir);
+	}
 
-    @AfterClass
-    public static void cleanupDatafiles() {
-        fragments.delete();
-    }
+	@BeforeClass
+	public static void setUpDatafiles() throws MalformedURLException, IOException {
+		PythonWrapperTestUtils.materializeKNIMEPythonUtils();
+		FileUtils.copyURLToFile(new URL("https://github.com/3D-e-Chem/kripodb/raw/master/data/fragments.sqlite"),
+				fragments);
+	}
 
-    @Test
-    public void test_default() throws IOException, InvalidSettingsException, CanceledExecutionException,
-            UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
-        File workflowDir = new File("src/knime/kripo-fragment-information-test-default");
-        runner.runTestWorkflow(workflowDir);
-    }
+	@AfterClass
+	public static void cleanupDatafiles() {
+		fragments.delete();
+	}
 
-    @Test
-    public void test_pdb() throws IOException, InvalidSettingsException, CanceledExecutionException,
-            UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
-        File workflowDir = new File("src/knime/kripo-fragment-information-test-pdb");
-        runner.runTestWorkflow(workflowDir);
-    }
+	@Test
+	public void test_default() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		runTestWorkflow("src/knime/kripo-fragment-information-test-default");
+	}
 
-    @Test
-    public void test_invalidsettings() throws IOException, InvalidSettingsException, CanceledExecutionException,
-            UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
-        File workflowDir = new File("src/knime/kripo-fragment-information-test-invalidsettings");
-        runner.runTestWorkflow(workflowDir);
-    }
+	@Test
+	public void test_pdb() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		runTestWorkflow("src/knime/kripo-fragment-information-test-pdb");
+	}
+
+	@Test
+	public void test_invalidsettings() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		runTestWorkflow("src/knime/kripo-fragment-information-test-invalidsettings");
+	}
 }
