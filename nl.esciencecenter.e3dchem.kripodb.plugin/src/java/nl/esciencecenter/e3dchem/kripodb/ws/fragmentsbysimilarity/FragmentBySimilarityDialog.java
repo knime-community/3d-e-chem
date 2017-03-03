c1d0d8a0dd819dfbd1e72da6ce1911f6ab6deaef
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-03-03 10:51:24 +0100

    Removed matrix setting from similarity ws node
    Its redudant because its superclass already implements it.
    
    
    (This commit was moved from https://github.com/3D-e-Chem/knime-kripodb. Original id: 17d4ac1d2c81083f62dd2ff3adeb743a4b163cda)

--- nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/ws/fragmentsbysimilarity/FragmentBySimilarityDialog.java
+++ nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/ws/fragmentsbysimilarity/FragmentBySimilarityDialog.java
@@ -13,29 +13,31 @@ import nl.esciencecenter.e3dchem.kripodb.ws.WsNodeDialog;
 /**
  * <code>NodeDialog</code> for the "FragmentBySimilarity" Node.
  *
- * This node dialog derives from {@link DefaultNodeSettingsPane} which allows creation of a simple dialog with standard
- * components. If you need a more complex dialog please derive directly from {@link org.knime.core.node.NodeDialogPane}.
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
+ * {@link org.knime.core.node.NodeDialogPane}.
  */
 public class FragmentBySimilarityDialog extends WsNodeDialog {
-    /**
-     * New pane for configuring FragmentBySimilarity node dialog. This is just a suggestion to demonstrate possible default dialog
-     * components.
-     */
-    protected FragmentBySimilarityDialog() {
-        super();
-        FragmentsBySimilarityConfig config = new FragmentsBySimilarityConfig();
+	/**
+	 * New pane for configuring FragmentBySimilarity node dialog. This is just a
+	 * suggestion to demonstrate possible default dialog components.
+	 */
+	protected FragmentBySimilarityDialog() {
+		super();
+		FragmentsBySimilarityConfig config = new FragmentsBySimilarityConfig();
 
-        SettingsModelString fragmentIdColumn = config.getFragmentIdColumn();
-        addDialogComponent(
-                new DialogComponentColumnNameSelection(fragmentIdColumn, "Fragment identifiers", 0, StringValue.class));
+		SettingsModelString fragmentIdColumn = config.getFragmentIdColumn();
+		addDialogComponent(
+				new DialogComponentColumnNameSelection(fragmentIdColumn, "Fragment identifiers", 0, StringValue.class));
 
-        SettingsModelDoubleBounded cutoff = config.getCutoff();
-        addDialogComponent(new DialogComponentNumber(cutoff, "Cutoff", 0.01));
+		SettingsModelDoubleBounded cutoff = config.getCutoff();
+		addDialogComponent(new DialogComponentNumber(cutoff, "Cutoff", 0.01));
 
-        SettingsModelIntegerBounded limit = config.getLimit();
-        addDialogComponent(new DialogComponentNumber(limit, "Limit", 1));
-        
-        createNewTab("Advanced");
-        addAdvancedSettings();
-    }
+		SettingsModelIntegerBounded limit = config.getLimit();
+		addDialogComponent(new DialogComponentNumber(limit, "Limit", 1));
+
+		createNewTab("Advanced");
+		addAdvancedSettings();
+	}
 }
--- nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/ws/fragmentsbysimilarity/FragmentsBySimilarityConfig.java
+++ nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/ws/fragmentsbysimilarity/FragmentsBySimilarityConfig.java
@@ -1,7 +1,5 @@
 package nl.esciencecenter.e3dchem.kripodb.ws.fragmentsbysimilarity;
 
-import java.io.File;
-
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
@@ -17,18 +15,14 @@ public class FragmentsBySimilarityConfig extends WsNodeConfig {
     private static final String CFG_FRAGIDCOLNAME = "fragment_id_column";
     private static final double DEFAULT_CUTOFF = 0.55;
     private static final String CFG_CUTOFF = "cutoff";
-    private static final String CFG_MATRIX = "matrix";
-    public static final String DEFAULT_MATRIX = "http://3d-e-chem.vu-compmedchem.nl/kripodb";
     private SettingsModelIntegerBounded m_limit = new SettingsModelIntegerBounded(CFG_LIMIT, DEFAULT_LIMIT, 0, Integer.MAX_VALUE);
     private SettingsModelString m_fragmentIdColumn = new SettingsModelString(CFG_FRAGIDCOLNAME, null);
     private SettingsModelDoubleBounded m_cutoff = new SettingsModelDoubleBounded(CFG_CUTOFF, DEFAULT_CUTOFF, 0.45, 1.0);
-    private SettingsModelString m_matrix = new SettingsModelString(CFG_MATRIX, DEFAULT_MATRIX);
 
     @Override
 	public void saveSettingsTo(NodeSettingsWO settings) {
 		super.saveSettingsTo(settings);
         m_fragmentIdColumn.saveSettingsTo(settings);
-        m_matrix.saveSettingsTo(settings);
         m_cutoff.saveSettingsTo(settings);
         m_limit.saveSettingsTo(settings);
 	}
@@ -37,20 +31,6 @@ public class FragmentsBySimilarityConfig extends WsNodeConfig {
 	public void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
 		super.loadValidatedSettingsFrom(settings);
         m_fragmentIdColumn.loadSettingsFrom(settings);
-        m_matrix.loadSettingsFrom(settings);
-        String matrix = m_matrix.getStringValue();
-        if ("".equals(matrix) || matrix == null) {
-            throw new InvalidSettingsException("Matrix file or ws url can not be empty");
-        } else {
-            if (matrix.startsWith("http")) {
-                // TODO test if webservice is online, using a HEAD request.
-            } else {
-                File fragmentsdb = new File(matrix);
-                if (!fragmentsdb.canRead()) {
-                    throw new InvalidSettingsException("Unable to read matrix file");
-                }
-            }
-        }
         m_cutoff.loadSettingsFrom(settings);
         m_limit.loadSettingsFrom(settings);
 	}
@@ -59,7 +39,6 @@ public class FragmentsBySimilarityConfig extends WsNodeConfig {
 	public void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
 		super.validateSettings(settings);
         m_fragmentIdColumn.validateSettings(settings);
-        m_matrix.validateSettings(settings);
         m_cutoff.validateSettings(settings);
         m_limit.validateSettings(settings);
 	}
@@ -81,9 +60,5 @@ public class FragmentsBySimilarityConfig extends WsNodeConfig {
 
     public SettingsModelDoubleBounded getCutoff() {
         return m_cutoff;
-    }
-
-    public SettingsModelString getMatrix() {
-        return m_matrix;
     }
 }
