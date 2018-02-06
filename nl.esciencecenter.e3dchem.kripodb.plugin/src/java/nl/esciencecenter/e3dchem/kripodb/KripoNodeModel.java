aba46e5c727bbee51d9849c3407e9e5d14cc78f1
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-02-06 14:37:17 +0100

    Update node description and missing kripodb python package error
    (This commit was moved from https://github.com/3D-e-Chem/knime-kripodb. Original id: 4f1886653a44885a352608ef40b5c75eba21ac7d)

--- nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/KripoNodeModel.java
+++ nl.esciencecenter.e3dchem.kripodb.plugin/src/java/nl/esciencecenter/e3dchem/kripodb/KripoNodeModel.java
@@ -1,5 +1,9 @@
 package nl.esciencecenter.e3dchem.kripodb;
 
+import java.io.IOException;
+
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.port.PortType;
 
 import nl.esciencecenter.e3dchem.python.PythonWrapperNodeConfig;
@@ -9,4 +13,18 @@ public abstract class KripoNodeModel<C extends PythonWrapperNodeConfig> extends 
 	public KripoNodeModel(PortType[] inPortTypes, PortType[] outPortTypes) {
 		super(inPortTypes, outPortTypes);
 	}
+
+	@Override
+	public BufferedDataTable[] execute(BufferedDataTable[] inData, ExecutionContext exec) throws Exception {
+		try {
+			return super.execute(inData, exec);
+		} catch (IOException e) {
+			if (e.getMessage().contains("kripodb")) {
+				throw new IOException(e.getMessage() + "See node description how to resolve.");
+			} else {
+				throw e;
+			}
+		}
+	}
+
 }
